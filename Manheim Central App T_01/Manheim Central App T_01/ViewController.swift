a660041d3fc26b41069d629e119a4efd8b798152
KBouder <kylebouder@gmail.com>
2015-03-17 13:57:17 -0400

    Initialization

---
+++ Manheim Central App T_01/Manheim Central App T_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manheim Central App T_01
+//
+//  Created by Bouder, Kyle T on 3/17/15.
+//  Copyright (c) 2015 Bouder, Kyle T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
